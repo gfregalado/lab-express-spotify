9471fd53b5a3e43ebe8adfa4849cd589709292fd
Gonçalo Regalado <gf.regalado@gmail.com>
2019-11-22 12:31:07 +0000

    had some issues with getting the info from the api but I am not even sure how I corrected it

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,19 +2,67 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
+const SpotifyWebApi = require('spotify-web-api-node');
+const path = require('path')
 const app = express();
+
+
+
+//viewengine
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 // setting the spotify-api goes here:
 
+const clientId = '555d994836b64552b2930488bb3f77ee';
+const clientSecret = '05fd7eaa407c42d4b0d50240da6df9c6';
+
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+// Retrieve an access token
+
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
+
 // the routes go here:
 
-app.listen(3000, () =>
-  console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
+app.get('/', (req, res, next) => {
+  res.render('index')
+})
+
+app.get('/artists', (req, res) => {
+  let searchedArtist = req.query.search;
+  spotifyApi.searchArtists(searchedArtist)
+    .then(data => {
+      console.log("The received data from the API: ", data.body.artists.items);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.render('artists.hbs', {
+        artistsObj: data.body.artists.items
+      })
+
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    });
+
+  //res.render("artists.hbs")
+})
+
+
+
+app.listen(3001, () =>
+  console.log('My Spotify project running on port 3001 🎧 🥁 🎸 🔊')
 );
