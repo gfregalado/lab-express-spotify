2390200a3765d0d51a2c7ef95599709ba8a2f7c1
Gonçalo Regalado <gf.regalado@gmail.com>
2019-11-23 12:17:55 +0000

    all the iterations done, missing styling

--- starter_code/app.js
+++ starter_code/app.js
@@ -13,21 +13,19 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-hbs.registerPartials(path.join(__dirname, "views/partials"));
+// connects partial views to fill the layout
+hbs.registerPartials(path.join(__dirname, "/views/partials"));
 
 // setting the spotify-api goes here:
-
-const clientId = '555d994836b64552b2930488bb3f77ee';
-const clientSecret = '05fd7eaa407c42d4b0d50240da6df9c6';
-
-
-const spotifyApi = new SpotifyWebApi({
+let clientId = 'b7f166ce91a04c77a17be089c1655d02',
+  clientSecret = 'e24445801730472fae96ed9d108b4262';
+// Create the api object with the credentials
+let spotifyApi = new SpotifyWebApi({
   clientId: clientId,
-  clientSecret: clientSecret
+  clientSecret: clientSecret,
 });
 
 // Retrieve an access token
-
 spotifyApi
   .clientCredentialsGrant()
   .then(data => {
@@ -37,31 +35,59 @@ spotifyApi
     console.log('Something went wrong when retrieving an access token', error);
   });
 
-// the routes go here:
 
+// the routes go here:
 app.get('/', (req, res, next) => {
   res.render('index')
 })
 
+// artists route
 app.get('/artists', (req, res) => {
   let searchedArtist = req.query.search;
   spotifyApi.searchArtists(searchedArtist)
     .then(data => {
       console.log("The received data from the API: ", data.body.artists.items);
-      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
       res.render('artists.hbs', {
+        //will become this. in the partials views as set in /artists route
         artistsObj: data.body.artists.items
       })
 
     })
     .catch(err => {
-      console.log("The error while searching artists occurred: ", err);
+      //console.log("The error while searching artists occurred: ", err);
     });
-
-  //res.render("artists.hbs")
 })
 
+// Albums route
+app.get('/albums/:artistId', (req, res, next) => {
+  let searchedAlbum = req.params.artistId;
+  spotifyApi.getArtistAlbums(searchedAlbum)
+    .then(data => {
+      console.log('Artist albums', data.body.items)
+      res.render('albums.hbs', {
+        albumObj: data.body.items
+      })
+    })
+    .catch(err => {
+      console.log("The error while searching them album occurred: ", err);
+    });
+})
 
+// Tracks route
+app.get('/tracks/:trackId', (req, res, next) => {
+  let searchedTracks = req.params.trackId;
+  console.log(searchedTracks)
+  spotifyApi.getAlbumTracks(searchedTracks)
+    .then(data => {
+      console.log('Album Tracks', data.body.items)
+      res.render('tracks.hbs', {
+        tracksObj: data.body.items
+      })
+    })
+    .catch(err => {
+      console.log("The error while searching them tracks occurred: ", err);
+    });
+})
 
 app.listen(3001, () =>
   console.log('My Spotify project running on port 3001 🎧 🥁 🎸 🔊')
